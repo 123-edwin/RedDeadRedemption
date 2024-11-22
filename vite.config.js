074eb5962a7c799171987c8cf76debc6ea549e41
robacodigo87 <erios8@ucol.mx>
2024-11-22 14:46:13 -0600

    Gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://123-edwin.github.io/RedDeadRedemption",
 })
